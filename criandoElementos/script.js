f611472f7f4790ae684ac68b578f568e9f1ab873
Cristiano Agnaldo Mulinari <62948211+Crismulinari@users.noreply.github.com>
2023-11-08 10:32:13 -0300

    projetoProdutos

--- criandoElementos/script.js
+++ criandoElementos/script.js
@@ -1,30 +1,22 @@
-// Criando um elemento para o título
-let titulo = document.createElement("h1");
 
-// Manipulando o elemento
+let titulo = document.createElement("h1");
 titulo.id = "titulo";
 titulo.innerText = "Armarinho";
 
-// Capturando o "elemento pai" dos elementos 'titulo' e 'produto'
-let body = document.querySelector("body");
 
-// Adicionando o elemento 'titulo' no DOM
+let body = document.querySelector("body");
 body.appendChild(titulo);
 
-// ------------------------------------------- //
 
-// Criando um elemento para o produto
-let produto = document.createElement("section");
-
-// Manipulando o elemento
-produto.innerHTML = `
+let produtos = document.createElement("section");
+produtos.innerHTML = `
   <section>
     <h2>Fios de seda</h2>
-    <img src="https://i.ytimg.com/vi/zlZEDtopR8Q/hqdefault.jpg" alt="x-salada">
+    <img src="../assets/imagens/Fio seda.jpg" alt="fios de seda coloridos">
     <p>Fio de seda colorido com expessura de número 10.</p>
     <p id="precoFioColorido10">R$ 25.30</p>
   </section>
 `;
 
-// Adicionando o elemento no dom
-body.appendChild(produto)
+
+body.appendChild(produtos)
